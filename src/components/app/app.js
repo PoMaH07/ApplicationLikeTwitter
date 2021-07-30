90e7ab6df721d40f6be511ab838178385e810b8f
Roman <gio92641@gmail.com>
2021-07-30 11:16:42 +0300

    Add Event Listener to posts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,13 @@ import PostAddForm from '../post-add-form';
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'fdsf'},
+        {label: 'That is so good', important: false, id: 'qwee'},
+        {label: 'I need a break...', important: false, id: 'klkk'}
+    ];
+
     return (
         <div className="app">
             <AppHeader />
@@ -15,7 +22,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
